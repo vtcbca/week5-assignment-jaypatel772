9d9b7531ce67e602ef50052f44670d68daf68e32
jaypatel772 <141392182+jaypatel772@users.noreply.github.com>
2023-08-17 14:40:25 +0530

    Add files via upload

---
+++ Week5/code csv.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import csv
+
+
+# In[2]:
+
+
+with open ('c:\\sqlite3\\csv\\helloworld.txt','r',errors="igoners")as f:
+    c=f.read()
+    print(c)
+
+
+# In[ ]:
+
+
+
+
